9581ea3e9236c5e8c8ba5596b61a904434f73ed1
Courtney Brown <courtneybrown@Courtneys-MacBook-Pro.local>
2021-03-05 18:49:17 -0600

    added questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,4 +15,63 @@ function countdown() {
     }, 1000);
 }
 
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Many Logos",
+      "Hyper Text Markup Language",
+      "Hyper Text Market Lingo",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Corresponding Style Sheet",
+      "Collaborative Simple Sheet",
+      "Computer Set Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is a function that is passed into another function as an argument called?",
+    answer: "Callback function",
+    options: [
+      "Callback function",
+      "On-call function",
+      "Double function",
+      "Repeated function"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following is NOT a falsy value?",
+    answer: "true",
+    options: [
+      "0",
+      "false",
+      "true",
+      "null"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the following is the CSS property that sets how flex items are placed in the flex container defining the main axis and the direction?",
+    answer: "flex-direction",
+    options: [
+      "align-content",
+      "flex-direction",
+      "flex-wrap",
+      "justify-content"
+    ]
+  },
+];
+
 startBtn.onclick = countdown;
+

1b0295454ae5b37220225cb785f0bb6c775b4341
Courtney Brown <courtneybrown@Courtneys-MacBook-Pro.local>
2021-03-11 14:16:07 -0600

    showQuestion function and nextQuestion function done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
 var timerEl = document.getElementById('countdown');
 var startBtn = document.getElementById('start');
+var optionList = document.querySelector(".optionList");
+var i = 0;
+var messageText = "";
+var timeLeft = 75;
 
 function countdown() {
-    var timeLeft = 75;
-
     var timeInterval = setInterval(function() {
         if (timeLeft > 1) {
             timerEl.textContent = timeLeft;
@@ -73,5 +75,73 @@ let questions = [
   },
 ];
 
-startBtn.onclick = countdown;
+function showQuestion() {
+    var quizContainerEl = document.createElement("div");
+    quizContainerEl.className = "quiz-container";
+    document.getElementById("page").append(quizContainerEl);
+
+    var submitMessage = document.createElement("div");
+    submitMessage.className = "submit-message";
+    submitMessage.innerHTML = messageText;
+    quizContainerEl.append(submitMessage);
+
+    var questionEl = document.createElement("div");
+    questionEl.className = "question-element";
+    questionEl.innerHTML = questions[i].question;
+    quizContainerEl.append(questionEl);
+
+    for (j=0; j < questions[i].options.length; j++) {
+        var optionEl = document.createElement("li");
+        optionEl.className = "option-element";
+        optionEl.innerHTML = questions[i].options[j];
+        quizContainerEl.append(optionEl);
+        optionEl.onclick = nextQuestion;
+    }
+
+   
+
+    function nextQuestion(event) {
+        if (event.target.innerText === questions[i].answer) {
+            console.log("true");
+            messageText = "Correct!";
+        } else {
+            console.log("false");
+            messageText = "Incorrect :( ";
+            timeLeft = timeLeft - 5;
+        }
+
+        i++;
+        quizContainerEl.remove();
+        showQuestion();
+        console.log(i);
+    }
+}
+
+
+
+function startQuiz() {
+    var startPage = document.getElementById("start-page");
+    startPage.remove();
+    countdown();
+    showQuestion();
+
+}
+
+function endQuiz() {
+    console.log("done");
+
+    var endPage = document.createElement("div");
+    endPage.className = "end-page";
+    document.getElementById("page").append(endPage);
+
+    var endPageHeader = document.createElement("h1");
+    endPageHeader.innerText = "All Done!";
+    endPage.append(endPageHeader);
+
+    var endPageText = document.createElement("p");
+    endPageText.innerHTML = "Your score is " + timeLeft;
+
+}
+
+startBtn.onclick = startQuiz;
 
